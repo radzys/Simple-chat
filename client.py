f6d419e3288a5adf884a2ea934d88970ef2cc548
radzzzy <60270510+radzzzy@users.noreply.github.com>
2020-08-16 13:54:39 +0300

    Update client.py
    Security improvements

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ def socket_listening():
             data = client_socket.recv(1024).decode('utf-8')
             print(data)
     except ConnectionResetError:
-        print('Server does not response :(')
+        print('Server is not responding :(')
 
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,26 +22,36 @@ client_socket.bind(('', 0))
 print("Welcome to chat, please enter server's address")
 while True:
     try:  # Trying to connect with server
-        address = input()
-        server_address = (address, 8050)
-        socket.getaddrinfo(address, 8050)
+        while True:
+            address = input()
+            if sys.getsizeof(address) <= 64:  # Checking size of server address to avoid buffer overflow
+                server_address = (address, 8050)
+                socket.getaddrinfo(address, 8050)
+                break
+            else:
+                print('Your input is too big, try again')
         break
     except socket.gaierror:
         print("This server does not exist, try again")
 
 print('Enter your nickname')
-nickname = input()
+while True:
+    nickname = input()
+    if sys.getsizeof(nickname) <= 1024:  # Checking size of nickname to avoid buffer overflow
+        client_socket.sendto(f'{nickname} connected to server'.encode('utf-8'), server_address)
+        print(f'Welcome to server, {nickname}!')
+        break
+    else:
+        print('Your nickname is too big, try again')
 
-client_socket.sendto(f'{nickname} connected to server'.encode('utf-8'), server_address)
-print(f'Welcome to server, {nickname}!')
 
 thread = threading.Thread(target=socket_listening)
 thread.start()
-""" Using the threading is necessary to be able to listen to the server and the user simultaneously"""
+""" Using threads is necessary to simultaneously listen to the server and the user"""
 
 while True:
     message = input()
-    if sys.getsizeof(message) <= 1024:  # Checking size of message for avoid buffer overflow
+    if sys.getsizeof(message) <= 1024:  # Checking size of message to avoid buffer overflow
         client_socket.sendto(f'{nickname}: {message}'.encode('utf-8'), server_address)
     else:
-        print('Your message is too long, it will be not sent')
+        print('Your message is too big, it will be not sent')
